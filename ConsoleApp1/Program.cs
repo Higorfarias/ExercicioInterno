1073699adc3e860cdd100dab9c9a34d0f92f372b
Higor Farias <higornf2009@gmail.com>
2025-06-20 12:27:28 -0300

    Exercicio interno

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 3, 2, 1, 6, 0, 5 };
+
+        int raiz = array[0];
+        int raizIndex = 0;
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > raiz)
+            {
+                raiz = array[i];
+                raizIndex = i;
+            }
+        }
+
+        
+        Console.WriteLine("Raiz: " + raiz);
+
+        Console.WriteLine("Galhos da esquerda");
+        for (int i = raizIndex = 1; i >= 0; i--)
+        {
+            Console.WriteLine(array[i]);
+        }
+        Console.WriteLine("Galhos da direita");
+        for (int i = raizIndex + 1;  i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+}
