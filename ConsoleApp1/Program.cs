1de1a775e456388b63d874023043b3be1188e22b
Higor Farias <higornf2009@gmail.com>
2025-06-20 17:53:41 -0300

    Ajustando os cenários

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 
-
-class Program
+class ArrayTest
 {
     static void Main()
     {
-        int[] array = { 3, 2, 1, 6, 0, 5 };
+        //  CENÁRIO 1 
+        int[] array1 = { 3, 2, 1, 6, 0, 5 };
+        Console.WriteLine("CENÁRIO 1:");
+        ProcessarArray(array1);
 
+        //  CENÁRIO 2 
+        int[] array2 = { 7, 5, 13, 9, 1, 6, 4 };
+        Console.WriteLine("\nCENÁRIO 2:");
+        ProcessarArray(array2);
+    }
+
+    static void ProcessarArray(int[] array)
+    {
         int raiz = array[0];
         int raizIndex = 0;
 
+        // pega a raiz de maior valor
         for (int i = 1; i < array.Length; i++)
         {
             if (array[i] > raiz)
@@ -19,18 +30,31 @@ class Program
             }
         }
 
-        
         Console.WriteLine("Raiz: " + raiz);
 
-        Console.WriteLine("Galhos da esquerda");
-        for (int i = raizIndex = 1; i >= 0; i--)
+       
+        int[] esquerda = new int[raizIndex];
+        Array.Copy(array, 0, esquerda, 0, raizIndex);
+        Array.Sort(esquerda);
+        Array.Reverse(esquerda);
+
+        Console.WriteLine("Galhos da Esquerda:");
+        foreach (int num in esquerda)
         {
-            Console.WriteLine(array[i]);
+            Console.Write(num + " ");
         }
-        Console.WriteLine("Galhos da direita");
-        for (int i = raizIndex + 1;  i < array.Length; i++)
+        Console.WriteLine();
+
+        int[] direita = new int[array.Length - raizIndex - 1];
+        Array.Copy(array, raizIndex + 1, direita, 0, direita.Length);
+        Array.Sort(direita);
+        Array.Reverse(direita);
+
+        Console.WriteLine("Galhos da Direita:");
+        foreach (int num in direita)
         {
-            Console.WriteLine(array[i]);
+            Console.Write(num + " ");
         }
+        Console.WriteLine();
     }
 }
